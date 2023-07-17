e991c0d6c4122f50d422cced359b1a926157b8ed
Marcos López C <MarcosLopezC@live.com>
2023-07-17 15:58:56 -0400

    Remove aliases

--- universal_aliases.sh
+++ universal_aliases.sh
@@ -1,9 +1,2 @@
-alias ea="code -w ~/.bash_aliases && source ~/.bash_aliases"
-alias gea="code -w ~/.gitconfig"
-
-alias b="cd .."
-
-alias c="code ."
-
 alias l="ls -lohv --time-style=long-iso --group-directories-first --color=auto"
 alias ll="ls -Alohv --time-style=long-iso --group-directories-first --color=auto"
